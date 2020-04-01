89957be1cf3deb8559028c878736dfbc26b65bdb
laurageeme <registro.laura@gmail.com>
2020-04-01 12:19:35 +0200

    component header and footer done

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,8 @@
 import React from 'react';
 import '../stylesheets/App.scss';
 import Header from './Header';
-import '../stylesheets/layouts/_header.scss';
+import Footer from './Footer';
+
 
 
 class App extends React.Component {
@@ -14,6 +15,7 @@ class App extends React.Component {
     return (
       <div className="App">
         <Header />
+        <Footer />
 
       </div>
     );
