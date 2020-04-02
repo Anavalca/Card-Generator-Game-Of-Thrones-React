9bc6e31daa1003aac8bb73c39109d8df5756356e
Lili Rascon <lilianarasconestrada@gmail.com>
2020-04-03 00:20:41 +0200

    first commit share

--- src/components/Form-components/ShareFormValidation.js
+++ src/components/Form-components/ShareFormValidation.js
@@ -1,1 +1,23 @@
 import React from 'react';
+import './_form-share.scss';
+
+class ShareFormValidation extends React.Component {
+
+    render() {
+        return (
+
+            <div className="share-form-validation">
+                <button type="button" className="panel__coll btn-share"><i class="fas fa-share-alt"></i>Comparte</button>
+                <div class="panel__content display__none">
+                    <button disabled type="button" class="panel__coll--child btn-create button__hover--styles" id="createCardButton"><i
+                        class="far fa-address-card"></i>Crear tarjeta</button>
+
+                    <span className="error-message errorEmail">*Faltan campos por completar*</span>
+                </div>
+            </div>
+        );
+
+    }
+}
+
+export default ShareFormValidation;
--- src/components/Form-components/ShareTwitter.js
+++ src/components/Form-components/ShareTwitter.js
@@ -1,1 +1,21 @@
 import React from 'react';
+import './_form-share.scss';
+
+class ShareTwitter extends React.Component {
+    render() {
+        return (
+
+            <div className="panel__content2 hidden">
+                <p id="success-message" className="success-message display__none">La tarjeta ha sido creada:</p>
+                <a className="print__url--twitter" href='' target="local"></a>
+                <a type="button" class="btn-twit button__hover--styles display__none" target="local">
+            
+                    <i class="fab fa-twitter" href=""></i>Compartir en Twitter</a>
+            </div>
+
+        );
+    }
+}
+
+
+export default ShareTwitter;
