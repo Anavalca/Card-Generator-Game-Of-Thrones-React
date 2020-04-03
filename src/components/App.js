ffbbc2e463000b18595e2416baaf688eb7670cbc
mgracesb <mercysbarcel@gmail.com>
2020-04-03 11:09:35 +0200

    minor changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,9 @@
 /* eslint-disable no-useless-constructor */
 import React from "react";
 import "../stylesheets/App.scss";
-import Header from './Header';
+import Header from "./Header";
 import FillForm from "./Form-components/FillForm";
-import Footer from './Footer';
-
-
+import Footer from "./Footer";
 
 class App extends React.Component {
   constructor(props) {
