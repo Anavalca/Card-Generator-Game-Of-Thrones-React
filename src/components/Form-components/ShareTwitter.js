7a6e716a977cadfd4d5f84c6af3bd87beb0d816e
Lili Rascon <lilianarasconestrada@gmail.com>
2020-04-03 20:22:48 +0200

    changes share

--- src/components/Form-components/ShareTwitter.js
+++ src/components/Form-components/ShareTwitter.js
@@ -6,7 +6,7 @@ class ShareTwitter extends React.Component {
         return (
 
             <div className="panel__content2 hidden">
-                <p id="success-message" className="success-message display__none">La tarjeta ha sido creada:</p>
+                <p id="success-message" className="success-message" className="display__none">La tarjeta ha sido creada:</p>
                 <a className="print__url--twitter" href='' target="local"></a>
                 <a type="button" className="btn-twit button__hover--styles display__none" target="local">
                 <i className="fab fa-twitter" href=""></i>Compartir en Twitter</a>
