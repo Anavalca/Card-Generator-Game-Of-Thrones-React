db1963745de210374fe89406a534ac79b715f387
mgracesb <mercysbarcel@gmail.com>
2020-04-04 16:15:50 +0200

    minor edit Landing

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,8 @@
 /* eslint-disable no-useless-constructor */
 import React from "react";
-// import "../stylesheets/App.scss";
-import Landing from "./Landing";
-import "../stylesheets/Landing.scss";
+import "../stylesheets/App.scss";
+// import Landing from "./Landing";
+// import "../stylesheets/Landing.scss";
 import Header from "./Header";
 import Main from "./Main";
 import Footer from "./Footer";
@@ -15,10 +15,10 @@ class App extends React.Component {
   render() {
     return (
       <div className="page-home">
-        <Landing />
-        {/* <Header />
+        {/* <Landing /> */}
+        <Header />
         <Main />
-        <Footer /> */}
+        <Footer />
       </div>
     );
   }
