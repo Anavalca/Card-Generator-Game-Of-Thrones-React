57dba23c6f9173eb4ddccd973941d02f64a658f2
mgracesb <mercysbarcel@gmail.com>
2020-04-04 14:08:43 +0200

    create landing structure and sass

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 /* eslint-disable no-useless-constructor */
 import React from "react";
 import "../stylesheets/App.scss";
-import Header from "./Header";
-import DesignForm from "./Form-components/DesignForm";
-import FillForm from "./Form-components/FillForm";
-import Footer from "./Footer";
+import "../stylesheets/Landing.scss";
+// import Header from "./Header";
+// import DesignForm from "./Form-components/DesignForm";
+// import FillForm from "./Form-components/FillForm";
+// import Footer from "./Footer";
+import Landing from "./Landing";
 
 class App extends React.Component {
   constructor(props) {
@@ -14,10 +16,11 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <Header />
+        <Landing />
+        {/* <Header />
         <DesignForm />
         <FillForm />
-        <Footer />
+        <Footer />*/}
       </div>
     );
   }
