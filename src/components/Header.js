ac509a96840d34042ede58d51f4b5dccadae2b11
AnaValca <avc92j@gmail.com>
2020-04-16 13:02:32 +0200

    add new logo and audio loop

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,38 +3,36 @@ import { Link } from 'react-router-dom';
 import logoCode from '../images/logo_motherCode.png';
 import audio from '../audio/game-thrones-song.mp3';
 
-class Header extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+const Header = (props) => {
 
-  render() {
-    return (
-      <div className="page__header">
-        <div className="logo__container">
-          <Link to="/" title="Ir a home">
-            <img src={logoCode} alt="Logo APC" />
-          </Link>
-        </div>
+  return (
+    <div className="page__header">
+      <div className="logo__container">
+        <Link to="/" title="Ir a home">
+          <img src={logoCode} alt="Logo APC" />
+        </Link>
+      </div>
+      <div className='extraContainer'>
         <div className="darkModeContainer">
           <input
             type="checkbox"
             name="checkbox"
             className="darkMode"
             id="darkMode"
-            // checked={props.value}
-            // onChange={props.switchHandler}
+            checked={props.value}
+            onChange={props.switchHandler}
           />
           <label htmlFor="darkMode"></label>
         </div>
         <div key='1' className='song'>
-          <audio controls autoPlay>
+          <audio controls autoPlay loop>
             <source src={audio} type='audio/mp3' />
           </audio>
         </div>  
       </div>
-    );
-  }
+    </div>
+  );
 }
+
 
 export default Header;
