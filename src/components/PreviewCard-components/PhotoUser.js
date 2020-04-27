2476232fc5ace9956e74c140613d36226673d420
mgracesb <mercysbarcel@gmail.com>
2020-04-27 12:50:28 +0200

    final final final

--- src/components/PreviewCard-components/PhotoUser.js
+++ src/components/PreviewCard-components/PhotoUser.js
@@ -1,20 +1,19 @@
 /* eslint-disable no-useless-constructor */
 import React from "react";
-// import defaultImageDarkMode from "../../images/jonsnow.gif";
+import defaultImageDarkMode from "../../images/jonsnow.gif";
 
 const PhotoUser = (props) => {
   let photo = props.photo;
-  // let darkModeValue = props.darkModeValue;
+  let darkModeValue = props.darkModeValue;
 
   return (
     <div
       className={"preview__card--picture"}
-      style={{ backgroundImage: `url(${photo})` }}
-      // style={
-      //   darkModeValue !== false && photo.length <= 35
-      //     ? { backgroundImage: `url(${defaultImageDarkMode})` }
-      //     : { backgroundImage: `url(${photo})` }
-      // }
+      style={
+        darkModeValue !== false && photo.length <= 35
+          ? { backgroundImage: `url(${defaultImageDarkMode})` }
+          : { backgroundImage: `url(${photo})` }
+      }
     >
       <canvas
         id="canvas"
