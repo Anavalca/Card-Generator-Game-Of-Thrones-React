2bb2f2c6cb61b1ae730519a5c34e3ff9831eaf01
mgracesb <mercysbarcel@gmail.com>
2020-04-27 10:51:46 +0200

    create production files

---
+++ docs/precache-manifest.04acdae74af49c860c0947ab40b130f9.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db0e7fdbfb50ed88e5ff4ae23951f60c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bc49c9dd2c32c9bfe564",
+    "url": "./static/css/main.01ce1b2a.chunk.css"
+  },
+  {
+    "revision": "c2a197d2c3f7a9c1487d",
+    "url": "./static/js/2.fbbc3528.chunk.js"
+  },
+  {
+    "revision": "4651a60dd24a3d76949974fa9e435183",
+    "url": "./static/js/2.fbbc3528.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc49c9dd2c32c9bfe564",
+    "url": "./static/js/main.52cb85f5.chunk.js"
+  },
+  {
+    "revision": "c591dafadec252fdf7c5",
+    "url": "./static/js/runtime-main.b94e8d65.js"
+  },
+  {
+    "revision": "28808ac61ac6e6257de3c94a035b8c65",
+    "url": "./static/media/bg2.28808ac6.jpg"
+  },
+  {
+    "revision": "5e8e49e35bca5948c7e238149e12e34b",
+    "url": "./static/media/daenerys.5e8e49e3.gif"
+  },
+  {
+    "revision": "110fcdf2980c322baee70e482c647f22",
+    "url": "./static/media/dark.110fcdf2.svg"
+  },
+  {
+    "revision": "a9d27190f1782913ecc04a38a8e26e6a",
+    "url": "./static/media/game-thrones-song.a9d27190.mp3"
+  },
+  {
+    "revision": "0b14af973acd71ef1c5ad078a9b7d9ff",
+    "url": "./static/media/jonsnow.0b14af97.gif"
+  },
+  {
+    "revision": "1b0b5e719ac552fa6e66c62b400ad5c9",
+    "url": "./static/media/light.1b0b5e71.svg"
+  },
+  {
+    "revision": "73735f3d5edf7e1c6de178c902f5b339",
+    "url": "./static/media/loader.73735f3d.gif"
+  },
+  {
+    "revision": "7eb2f0e7d960fe55ab328a5459663ac6",
+    "url": "./static/media/logo_motherCode.7eb2f0e7.png"
+  },
+  {
+    "revision": "9c9344c9b7e810008314cf664d603d30",
+    "url": "./static/media/logo_motherCode_landing.9c9344c9.png"
+  },
+  {
+    "revision": "235765cecd3da1f96ea8c471db46d080",
+    "url": "./static/media/loop-peque.235765ce.gif"
+  }
+]);
